38fdb188bf50c579d362b21d6e5de582622c7b9b
nikita1984 <52231270+nikita1984@users.noreply.github.com>
2021-09-11 20:13:07 +0300

    create folderList

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import MessageList from "./MessageList";
+import MessageInput from "./MessageInput";
+import FolderList from "./FolderList.js";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  appWrapper: {
+    height: "95vh",
+    width: "100vw",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  componentWrapper: {
+    width: "600px",
+    height: "630px",
+    border: "1px solid black",
+    display: "flex",
+    flexDirection: "column",
+  },
+}));
+
+function App() {
+  const [messagesArray, setMessagesArray] = useState([]);
+
+  const classes = useStyles();
+
+  const onSendMessage = (messageText) => {
+    setMessagesArray((prev) => [...prev, messageText]);
+  };
+
+  return (
+    <div className={classes.appWrapper}>
+      <FolderList></FolderList>
+      <div className={classes.componentWrapper}>
+        <MessageList messagesArray={messagesArray} />
+        <MessageInput onSendMessage={onSendMessage} />
+      </div>
+    </div>    
+  );
+}
+
+export default App;
